6642d8628a81545e39df03803295f2588566065d
Max <rekaru@protonmail.ch>
2017-05-11 18:15:18 +0300

    Update app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import MessageField from './messageField.jsx';
 import InputField from './input.jsx';
+import PopUp from './popup.jsx';
 
 class App extends Component {
-    render() {
-        return(
-            <div>
-                <MessageField/>
-                <InputField/>
-            </div>
-        )
-    }
+  render() {
+    return(
+      <div>
+        <PopUp/>
+        <MessageField/>
+        <InputField/>
+      </div>
+    )
+  }
 }
 
 export default App;
