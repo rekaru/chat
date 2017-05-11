dfd53f6155b6f45b76ca66278356504ed44840b1
Max <rekaru@protonmail.ch>
2017-05-11 22:04:54 +0300

    Update input.jsx

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -5,13 +5,6 @@ const socket = io();
 
 class InputField extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: ''
-    }
-  }
-
   render() {
     let input;
     let emitMsg = {
@@ -19,29 +12,25 @@ class InputField extends Component {
       text: '',
     };
     return(
-      <div>
+      <div className="inputContainer">
         <form onSubmit={e => {
           e.preventDefault();
           if (!input.value.trim()) {
-            return
+            return;
           }
           emitMsg.text = input.value;
           socket.emit('chat message', emitMsg );
           input.value = '';
-        }} >
+        }}>
           <input ref={node => {
-            input = node
-          }} />
+            input = node;
+          }} placeholder="type something"/>
           <button type="submit">Send</button>
         </form>
       </div>
-    )
+    );
   }
 }
 
 
-const MapStateToProps = state => {
-  return state
-};
-
-export default connect(MapStateToProps)(InputField);
+export default connect(state => state)(InputField);
