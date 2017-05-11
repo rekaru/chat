9c1dd8c3cd682f52175918c95c5f3443647a486a
Max <rekaru@protonmail.ch>
2017-05-11 18:18:08 +0300

    Update messageField.jsx

--- src/components/messageField.jsx
+++ src/components/messageField.jsx
@@ -3,27 +3,26 @@ import { connect } from 'react-redux';
 import Message from './message.jsx';
 
 class MessageField extends Component {
-    render() {
-        let propsStore = this.props.messages;
-        console.log(propsStore);
-        let allMessages = propsStore.map(message => {
-            return (
-            <Message user={message.user} text={message.text} />
-            )
-        });
+  render() {
+    let propsStore = this.props.messages.messages;
+    let allMessages = propsStore.map(message => {
+      return (
+      <Message user={message.user} text={message.text} />
+      )
+    });
 
-        return (
-            <div>
-                {allMessages}
-            </div>
-        )
-    }
+    return (
+      <div>
+        {allMessages}
+      </div>
+    )
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        messages: state
-    }
-};
+  return {
+    messages: state
+  }
+}
 
 export default connect(mapStateToProps)(MessageField);
