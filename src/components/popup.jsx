d09d41dea91f4017edb8d3167b1d49a9edfef0bd
Max <rekaru@protonmail.ch>
2017-05-11 22:06:43 +0300

    Update popup.jsx

--- src/components/popup.jsx
+++ src/components/popup.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addUser } from '../actions/index.js';
 
-
 class PopUp extends Component {
 
   constructor(props) {
@@ -14,35 +13,31 @@ class PopUp extends Component {
   }
 
   onSubmit(event)  {
-    if (event.key === 'Enter') {
-      if (event.target.value) {
-        this.props.dispatch(addUser(event.target.value));
-        this.setState({
-          isComplete: true
-        });
-      }
+    if (event.key === 'Enter' && event.target.value) {
+      this.props.dispatch(addUser(event.target.value));
+      this.setState({
+        isComplete: true
+      });
     }
   };
 
 
   render() {
-    if (!this.state.isComplete) {
-      return (
-        <div className="popup">
-          <h3>What's your nickname?</h3>
-          <input className="popupInput" type="text" maxLength={14}
-          onKeyPress={this.onSubmit}/>
-        </div>
-      )
+    if (this.state.isComplete) {
+      return null;
     }
-    else {
-      return null
-    }
+    return (
+      <div className="popup">
+        <h3>What's your nickname?</h3>
+        <input className="popupInput" type="text" maxLength={14}
+               onKeyPress={this.onSubmit}/>
+      </div>
+    );
   }
 }
 
 const MapStateToProps = state => {
-  return state
-}
+  return state;
+};
 
 export default connect(MapStateToProps)(PopUp);
