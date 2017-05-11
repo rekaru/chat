974e4ad897786e5489ca697867ea2f178a107fac
Max <rekaru@protonmail.ch>
2017-05-11 22:05:29 +0300

    Update message.jsx

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,14 +1,14 @@
 import React, { Component} from 'react';
 
 class Message extends Component {
-    render() {
-        return (
-            <div>
-                <span>{this.props.user} : </span>
-                <span>{this.props.text}</span>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="message">
+        <span>{this.props.user} : </span>
+        <span>{this.props.text}</span>
+      </div>
+    );
+  }
 }
 
 export default Message;
