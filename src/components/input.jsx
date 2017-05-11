e7412e732a0f55745f081fd66fba485b33fd0649
Max <rekaru@protonmail.ch>
2017-05-11 18:16:44 +0300

    Update input.jsx

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,35 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addMsg } from '../actions/index.js';
-import io from 'socket.io-client';
+
+const socket = io();
 
 class InputField extends Component {
-    render() {
 
-        let input;
-        return(
-            <div>
-                <form onSubmit={e => {
-                    e.preventDefault();
-                    if (!input.value.trim()) {
-                        return
-                    }
-                    this.props.dispatch(addMsg(input.value));
-                    input.value = '';
-                }} >
-                    <input ref={node => {
-                        input = node
-                    }} />
-                    <button type="submit">Написать</button>
-                </form>
-            </div>
-        )
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
     }
+  }
+
+  render() {
+    let input;
+    let emitMsg = {
+      user: this.props.user,
+      text: '',
+    };
+    return(
+      <div>
+        <form onSubmit={e => {
+          e.preventDefault();
+          if (!input.value.trim()) {
+            return
+          }
+          emitMsg.text = input.value;
+          socket.emit('chat message', emitMsg );
+          input.value = '';
+        }} >
+          <input ref={node => {
+            input = node
+          }} />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 
-function MapStateToProps(state) {
-    return {state}
-}
+const MapStateToProps = state => {
+  return state
+};
 
 export default connect(MapStateToProps)(InputField);
